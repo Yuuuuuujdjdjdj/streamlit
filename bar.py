8ccf9556fbd593d8467c38e5399b7572b24e67a1
yumin1001 <162390237+Yuuuuuujdjdjdj@users.noreply.github.com>
2024-03-27 06:35:39 +0000

    l

--- bar.py
+++ bar.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
-rand=np.random.normal(1, 2,size=20)
+rand=np.random.normal(1, 2, size=20)
 fig, ax = plt.subplots()
-ax.hist(rand, bins=15) #, color="pink"
+ax.hist(rand, bins = 15)
 st.pyplot(fig)
