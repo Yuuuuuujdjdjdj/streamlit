d5c070b37d96ec81762d6250641e0687515de643
yumin1001 <162390237+Yuuuuuujdjdjdj@users.noreply.github.com>
2024-03-27 00:58:43 +0000

    no

--- bar.py
+++ bar.py
@@ -1,5 +1,6 @@
 import streamlit as st
-import matplotlib.pyplot as plt import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
 
 rand=np.random.normal(1, 2,size=20)
 fig, ax = plt.subplots()
